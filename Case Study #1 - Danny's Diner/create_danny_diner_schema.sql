088ff01711e39f4a71271053c7ce6d4fc5d319b0
LUCAS MASCARENHAS <lucasa88@braskem.com>
2022-09-14 09:20:28 -0300

    feat[first sql code]: show on README the sql tool that I will use (MySQL) and push the sql code to create the schema and to insert the data

---
+++ Case Study #1 - Danny's Diner/create_danny_diner_schema.sql
@@ -0,0 +1,65 @@
+/* 
+Case Study #1 - Danny's Diner
+Tool: MySQL
+*/
+
+-- CREATE SCHEMA/TABLES AND INSERT DATA
+CREATE DATABASE IF NOT EXISTS dannys_diner;
+USE dannys_diner;
+
+-- CREATE MENU TABLE
+-- DROP TABLE IF EXISTS menu;
+CREATE TABLE IF NOT EXISTS menu (
+	product_id INT,
+    product_name VARCHAR(5),
+    price INT
+);
+
+INSERT INTO 
+	menu (product_id, product_name, price)
+VALUES
+	(1, 'sushi', 10),
+    (2, 'curry', 15),
+    (3, 'ramen', 12);
+    
+-- CREATE MEMBERS TABLE
+-- DROP TABLE IF EXISTS members;
+CREATE TABLE IF NOT EXISTS members (
+	customer_id VARCHAR(1),
+    join_date DATE
+);
+
+INSERT INTO
+	members (customer_id, join_date)
+VALUES
+	('A', '2021-01-07'),
+    ('B', '2021-01-09');
+
+-- CREATE SALES TABLE
+-- DROP TABLE IF EXISTS sales;
+CREATE TABLE IF NOT EXISTS sales (
+	customer_id VARCHAR(1),
+    order_date DATE,
+    product_id INT
+);
+
+INSERT INTO 
+	sales (customer_id, order_date, product_id) 
+VALUES
+  ('A', '2021-01-01', '1'),
+  ('A', '2021-01-01', '2'),
+  ('A', '2021-01-07', '2'),
+  ('A', '2021-01-10', '3'),
+  ('A', '2021-01-11', '3'),
+  ('A', '2021-01-11', '3'),
+  ('B', '2021-01-01', '2'),
+  ('B', '2021-01-02', '2'),
+  ('B', '2021-01-04', '1'),
+  ('B', '2021-01-11', '1'),
+  ('B', '2021-01-16', '3'),
+  ('B', '2021-02-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-07', '3');
+    
+
