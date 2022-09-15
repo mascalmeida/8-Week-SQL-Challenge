2178b27b4aeece94d804f739c5d18d095f21f3d7
LUCAS MASCARENHAS <lucasa88@braskem.com>
2022-09-15 09:20:40 -0300

    feat[challenge 1]: solutions for questions 3, 4 and partial solution for question 5

--- Case Study #1 - Danny's Diner/solution_danny_diner_questions.sql
+++ Case Study #1 - Danny's Diner/solution_danny_diner_questions.sql
@@ -10,7 +10,7 @@ USE dannys_diner;
 -- 1. What is the total amount each customer spent at the restaurant?
 SELECT sales.customer_id, SUM(menu.price) as total_amount
 FROM sales
-INNER JOIN menu ON sales.product_id=menu.product_id
+INNER JOIN menu ON sales.product_id = menu.product_id
 GROUP BY customer_id
 ORDER BY total_amount DESC;
 
@@ -21,3 +21,36 @@ GROUP BY customer_id
 ORDER BY n_days DESC;
 
 -- 3. What was the first item from the menu purchased by each customer?
+SELECT sales.customer_id, MIN(sales.order_date) AS first_purchased, menu.product_name 
+FROM sales
+INNER JOIN menu ON sales.product_id = menu.product_id 
+GROUP BY customer_id;
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT MAX(n_purchased) AS max_n_purchased, results.product_name
+FROM (
+	SELECT COUNT(sales.product_id) AS n_purchased, menu.product_name
+	FROM sales
+	INNER JOIN menu ON sales.product_id = menu.product_id 
+	GROUP BY menu.product_name
+	ORDER BY n_purchased DESC
+	) AS results;
+
+-- 5. Which item was the most popular for each customer?
+-- Maybe create a new colum with the rank of each customer
+SELECT results.customer_id, MAX(n_purchased) AS max_n_purchased, results.product_name
+FROM (
+	SELECT sales.customer_id, COUNT(sales.product_id) AS n_purchased, menu.product_name
+	FROM sales
+	INNER JOIN menu ON sales.product_id = menu.product_id 
+	GROUP BY sales.customer_id, menu.product_name
+	ORDER BY n_purchased DESC
+	) AS results
+GROUP BY customer_id;
+
+
+
+
+
+
+
